2f03b9732dc4293040311fd3f198190e3bbdcec6
Alvaro Vanegas <alvaro.vanegas@globalmvm.com>
2025-11-18 21:43:02 -0500

    esqueleto

---
+++ CapacitacionInterna/src/main/java/com/Curso/CapacitacionInterna/CapacitacionInternaApplication.java
@@ -0,0 +1,13 @@
+package com.Curso.CapacitacionInterna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacionInternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitacionInternaApplication.class, args);
+	}
+
+}
